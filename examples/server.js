8bf30cbf1c17375d6140777270032c3ae0a98ad7
sheben404 <sheben404@gmail.com>
2021-04-10 17:27:50 +0800

    add buildURL

--- examples/server.js
+++ examples/server.js
@@ -33,6 +33,10 @@ router.get('/simple/get', function(req, res) {
 
 app.use(router)
 
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
+})
+
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
