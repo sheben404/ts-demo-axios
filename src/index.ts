3ce8e3e4a98e2857d020e9914959366407989263
sheben404 <sheben404@gmail.com>
2021-04-10 16:35:39 +0800

    first xhr demo

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-function axios(config) {
-  // Todo
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig): void {
+  xhr(config)
 }
 
 export default axios
