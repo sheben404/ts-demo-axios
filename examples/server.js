afb2ab2c8ef19c60975c95b03c20c380557597f5
sheben404 <sheben404@gmail.com>
2021-04-12 14:42:57 +0800

    响应数据支持泛型

--- examples/server.js
+++ examples/server.js
@@ -101,6 +101,17 @@ router.patch('/extend/patch', function(req, res) {
   res.json(req.body)
 })
 
+router.get('/extend/user', function (req, res) {
+  res.json({
+    code: 0,
+    message: 'ok',
+    result: {
+      name: 'Alice',
+      age: 18
+    }
+  })
+})
+
 app.use(router)
 
 const port = process.env.PORT || 8080
